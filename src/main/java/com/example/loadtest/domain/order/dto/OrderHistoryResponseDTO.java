bc04ed46193bda61fb64152299350c7606ac1bc9
JseonPark <jugal.park78@gmail.com>
2025-11-02 21:07:24 +0900

    유저 주문내역조회 API

--- src/main/java/com/example/loadtest/domain/order/dto/OrderHistoryResponseDTO.java
+++ src/main/java/com/example/loadtest/domain/order/dto/OrderHistoryResponseDTO.java
@@ -12,11 +12,17 @@ public class OrderHistoryResponseDTO {
   private final String storeName;
   private final String status;
   private final ZonedDateTime orderTime;
+  private final int totalPrice;
+  private final int discountAmount;
+  private final int paidPrice;
 
     public OrderHistoryResponseDTO(Order order) {
         this.orderSummaryText = order.getOrderSummaryText();
         this.storeName = order.getStore().getName();
         this.status = order.getStatus();
         this.orderTime = order.getOrderTime();
+        this.discountAmount = (order.getPayment() != null) ? order.getPayment().getDiscountAmount() : 0;
+        this.totalPrice = (order.getPayment() != null) ? order.getPayment().getTotalPaid() : 0;
+        this.paidPrice = this.totalPrice - this.discountAmount;
     }
 }
